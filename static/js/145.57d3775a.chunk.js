92b40d9a519b5c89b3f9661a58b3f0636a3569bf
Łukasz Boczoń <lukasz.boczon@gotoma.pl>
2022-10-07 19:50:19 +0200

    Updates

--- static/js/145.57d3775a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfifa_cup=self.webpackChunkfifa_cup||[]).push([[145],{3145:function(a,n,e){e.r(n);var c=e(5738),i=e(7689),u=e(184);n.default=function(){var a=(0,i.UO)().id,n=(0,c.Dq)(a,{enabled:!!a}),e=n.data;return n.isLoading,console.log(null===e||void 0===e?void 0:e.docs,"data"),(0,u.jsx)(u.Fragment,{children:"abc"})}}}]);
-//# sourceMappingURL=145.57d3775a.chunk.js.map
